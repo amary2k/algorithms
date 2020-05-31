91fdb0beeddfeb6ea0d21ecafa5647729a2a2667
Amar Agrawal <amar@Amars-MacBook-Air.local>
2020-05-30 22:55:25 -0400

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,72 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  numberArray: number[];
   title = 'sorting';
+  steps = [];
+  stylingArray = [];
+
+  @Input() arraySize: number;
+
+  ngOnInit(): void {
+    this.numberArray = this.initializeNumberArray(10);
+    this.shuffleArray(this.numberArray);
+    this.steps.push([...this.numberArray]);
+  }
+
+  onArraySizeChange(event) {
+    this.numberArray = this.initializeNumberArray(event.target.value);
+    this.shuffleArray(this.numberArray);
+    this.steps.push([...this.numberArray]);
+  }
+
+  shuffleArray (arrayToShuffle: any[])  {
+    for (let i = 0; i < arrayToShuffle.length; i++) {
+			let randomIndexToSwap = Math.floor(Math.random() * arrayToShuffle.length);
+			const temp = arrayToShuffle[randomIndexToSwap];
+			arrayToShuffle[randomIndexToSwap] = arrayToShuffle[i];
+			arrayToShuffle[i] = temp;
+		}
+  }
+
+  initializeNumberArray (lengthOfArray: number, start: number = 0): number[] {
+    let numberArray = [];
+    for(let i=start; i < lengthOfArray; i++) {
+      numberArray.push(i);
+    }
+    return numberArray;
+  }
+
+  sortArray (arrayToSort:number[]) {
+    // Bubble Sort
+    for (let i = 0; i < arrayToSort.length; i++) {
+      for (let j = 0; j < arrayToSort.length; j++) {
+        if(arrayToSort[j] > arrayToSort[j+1]) { // If greater then swap
+          this.swapArrayValuesAndRefresh(arrayToSort,j,j+1);
+        }
+      }
+    }
+  }
+
+  swapArrayValues (arrayToSwap:any[], index1:number, index2:number) {
+    const temp = arrayToSwap[index2];
+    arrayToSwap[index2] = arrayToSwap[index1];
+    arrayToSwap[index1] = temp;
+    return arrayToSwap;
+  }
+
+  swapArrayValuesAndRefresh (arrayToSwap:any[], index1:number, index2:number) {
+    this.swapArrayValues(arrayToSwap , index1 , index2 );
+    this.steps.push([...arrayToSwap]);
+    this.stylingArray.push([index1,index2]);
+  }
+
+  onSort() {
+    this.sortArray(this.numberArray);
+  }
 }
